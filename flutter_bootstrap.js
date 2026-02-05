7e6c3d9d53374544ad96f9e8ae10932bb537b803
yuanchaoguo392-commits <yuanchaoguo392@gmail.com>
2026-02-05 14:11:22 +0800

    修复3

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -52,7 +52,7 @@ if (flet.noCdn) {
 _flutter.loader.load({
     config: flutterConfig,
     serviceWorkerSettings: {
-        serviceWorkerVersion: "582449976",
+        serviceWorkerVersion: "1134078716",
     },
     onEntrypointLoaded: async function (engineInitializer) {
         const engine = await engineInitializer.initializeEngine(flutterConfig);
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,8 +3,8 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"assets/app/app.zip": "fed71dcfa10e46da040b8094daec0444",
-"assets/app/app.zip.hash": "e4a9192721e4530e98c02df29b48aaad",
+const RESOURCES = {"assets/app/app.zip": "673f6faccf2aa339ce7b6c6d5fb5b11d",
+"assets/app/app.zip.hash": "bccc6f7a87736efb111d9124dbcf0442",
 "assets/AssetManifest.bin": "5e4c69c57b629fa3c0fbf78c71c4db94",
 "assets/AssetManifest.bin.json": "992ba04d5892193176350dad1f95a2ec",
 "assets/FontManifest.json": "9bd568e67ff3c6c597e4f9494ef71249",
@@ -33,7 +33,7 @@ const RESOURCES = {"assets/app/app.zip": "fed71dcfa10e46da040b8094daec0444",
 "favicon.png": "41e5bc5ad6aab67633b4d31c0aa2b8ab",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "flutter.js.map": "b0053d0f522eea08c12809f1843c34c9",
-"flutter_bootstrap.js": "6746e6a29fdd870b31af31e58ac27d1a",
+"flutter_bootstrap.js": "e338d2776d6e6dc4754a08666870730a",
 "icons/apple-touch-icon-192.png": "8cf0d5162941f467a77f023c414a1812",
 "icons/Icon-192.png": "d1f96bab23c50fe5f5db429aabbac81a",
 "icons/Icon-512.png": "0b1dcfc1bd6c872904999125eaf18b58",
